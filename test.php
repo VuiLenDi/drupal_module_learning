d89c9f5980ed2aadc723e6685e70b8c147e2c700
Minh Tran <minh.tran@nashtechglobal.com>
2021-03-25 23:03:18 +0700

    Update README

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = \Drupal::service('config.factory')->getEditable('hello_world.settings');
-// Set and save new message value.
-$config->set('message', 'Hi')->save();
-// Now will print 'Hi'.
-print $config->get('message');
